92e6c3936ef4940ece2ccaaaaf51f4ed9288de0c
Endrit <enokryeziu@gmail.com>
2020-05-20 15:54:49 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Web Crawler JAVA
+
+TensorFlow, as you may have heard, is an open source library for machine learning, originally developed by Google. This app use a pretrained MobileNet model and train another model using an internal mobilenet activation to predict 4 different classes from the webcam defined by the user. 
+
+![Web Crawler](https://github.com/enokryeziu/java-web-crawler/blob/master/web-crawler.png)
+
+
+## Getting Started
+
+### Prerequisites
+
+```
+Web server or Firefox
+```
+
+### Installing
+
+1. Upload file to the directory of your choice.
+
+```
+git clone https://github.com/enokryeziu/java-web-crawler.git
+```
+
+2. To run this app you have to either set up a web server and host files via localhost or open with Firefox (feature support).
+
+## Version
+```
+Final Version 2020
+```
---
+++ js/index.js
@@ -0,0 +1,140 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById('webcam');
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+  var classArray = ["class-b", "class-c", "class-d"];
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+    //console.log(draw());
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+    if(classId !=3)
+      document.getElementById(classArray[classId]).disabled = false;
+    else {
+      document.getElementById("playButton").disabled = false;
+    }
+  };
+
+function draw() {
+
+    var hidden_canvas = document.querySelector('canvas'),
+      context = hidden_canvas.getContext('2d');
+      var width = webcamElement.videoWidth,
+      height = webcamElement.videoHeight;
+
+    if (width && height) {
+
+      // Setup a canvas with the same dimensions as the video.
+      hidden_canvas.width = width;
+      hidden_canvas.height = height;
+
+      // Make a copy of the current frame in the video on the canvas.
+      context.drawImage(webcamElement, 0, 0, width, height);
+
+      // Turn the canvas image into a dataURL that can be used as a src for our photo.
+      return hidden_canvas.toDataURL('image/png');
+    }
+  }
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
+  document.getElementById('playButton').addEventListener('click', () => startTheGame());
+  async function startTheGame() {
+    document.getElementById("help").outerHTML = "";
+    while (true) {
+      if (classifier.getNumClasses() > 0) {
+        // Get the activation from mobilenet from the webcam.
+        const activation = net.infer(webcamElement, 'conv_preds');
+        // Get the most likely class and confidences from the classifier module.
+        const result = await classifier.predictClass(activation);
+
+        const classes = ['Up', 'Left', 'Down', 'Right'];
+        document.getElementById('console').innerText = `
+          Prediction: ${classes[result.classIndex]}\n
+          Probability: ${(result.confidences[result.classIndex] * 100).toFixed(2)}%
+        `;
+        
+          var axis = [0, 0];
+
+          //up
+          if(classes[result.classIndex] == 'Up') {
+              if(axis[1] === 0) {
+                  axis[1] = 1;
+              }else  {
+              axis[1] = 0;
+              }
+          }   
+          //down
+          if(classes[result.classIndex] == 'Down') {
+              if(axis[1] === 0) {
+                  axis[1] = -1;
+              } else  {
+                  axis[1] = 0;
+              }
+          }   
+
+          //left
+          if(classes[result.classIndex] == 'Left') {
+              if(axis[0] === 0) {
+                  axis[0] = -1;
+              } else  {
+                  axis[0] = 0;
+              }
+          }  
+
+          //right
+          if(classes[result.classIndex] == 'Right') {
+              if(axis[0] === 0) {
+                  axis[0] = 1;
+              } else  {
+                  axis[0] = 0;
+              }
+          }  
+
+          var timer = setInterval(function(){
+              //run the callback
+              onMoveKey(axis);
+
+          }, 1);
+      }
+
+      await tf.nextFrame();
+    }
+  }
+}
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+app();
